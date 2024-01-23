9adb773ac763253a243dc1c98fa1f2fd1d50460c
Atendimento2005 <gogo.june2005@gmail.com>
2024-01-23 15:39:16 +0530

    update

--- sort.js
+++ sort.js
@@ -1,47 +1,42 @@
 function sortAll() {
+	let cardsContainers = document.querySelectorAll(".row.g-3");
 
-  let cardsContainers = document.querySelectorAll(".row.g-3")
+	cardsContainers.forEach((cardsContainer) => {
+		//Expand the chapter list
+		cardsContainer.lastChild.firstChild.click();
 
-  cardsContainers.forEach(cardsContainer => {
+		let data = Array.from(cardsContainer.children).sort((a, b) => {
+			// Extract unattempted and mistakes
+			let valueb = Array.from(b.querySelectorAll(".sc-gKXOVf.leoCAQ")).map(
+				(elem) => Number(elem.innerText.replace(/\D/g, ""))
+			);
+			let valuea = Array.from(a.querySelectorAll(".sc-gKXOVf.leoCAQ")).map(
+				(elem) => Number(elem.innerText.replace(/\D/g, ""))
+			);
 
-    //Expand the chapter list
-    cardsContainer.lastChild.firstChild.click()
+			mistakesa = valuea[0];
+			mistakesb = valueb[0];
+			unattempteda = valuea[1];
+			unattemptedb = valueb[1];
 
-    let data = Array.from(cardsContainer.children).sort((a, b) => {
-      // Calculate sum of unattempted and mistakes
-      let valueb = Array.from(b.querySelectorAll(".sc-gKXOVf.leoCAQ")).map(elem => Number(elem.innerText.replace(/\D/g,'')))
-      let valuea = Array.from(a.querySelectorAll(".sc-gKXOVf.leoCAQ")).map(elem => Number(elem.innerText.replace(/\D/g,'')))
-  
-      console.log(valuea)
+			if (mistakesa !== mistakesb) {
+				return mistakesa < mistakesb;
+			}
 
-      mistakesa = valuea[0]
-      mistakesb = valueb[0]
-      unattempteda = valuea[1]
-      unattemptedb = valueb[1]
+			return unattempteda < unattemptedb;
+		});
 
-      if (mistakesa !== mistakesb) {
-        return mistakesa > mistakesb
-      }
-      
-      return unattempteda > unattemptedb
+		cardsContainer.innerHTML = "";
 
-      
-    })
+		data.forEach((elem) => {
+			cardsContainer.appendChild(elem);
+		});
+	});
 
-    data.reverse()
-
-    cardsContainer.innerHTML = ''
-
-    data.forEach(elem => {
-      cardsContainer.appendChild(elem)
-    })
-
-  })
-
-  console.log("Executed!")
+	console.log("Executed!");
 }
 
 browser.runtime.onMessage.addListener((message) => {
-  console.log("Starting...")
-  sortAll()
-})
+	console.log("Starting...");
+	sortAll();
+});
